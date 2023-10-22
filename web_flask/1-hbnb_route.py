ae0afc33a1fb98397366bd9188b74dfed6e4a601
Abdullah Bedwey <bedweyy@gmail.com>
2023-10-22 11:26:25 -0700

    fix style

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,7 +2,6 @@
 """
 starts a Flask web application.
 """
-
 from flask import Flask
 app = Flask(__name__)
 
@@ -12,10 +11,12 @@ def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """returns HBNB"""
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
