1c42cc5c713debc05b2a95f7a9d95ef93d641431
Abdullah Bedwey <bedweyy@gmail.com>
2023-10-22 11:36:16 -0700

    fix styles

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -33,9 +33,9 @@ def pythoniscool(text='is cool'):
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def printnumber(n):
-	"""display “n is a number” only if n is an integer"""
-	return "{:d} is a number".format(n)
+    """display “n is a number” only if n is an integer"""
+    return "{:d} is a number".format(n)
 
- 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
